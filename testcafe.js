d5594201607474e1dd833e5ee350818bc3209a5c
Eugene Titerman <eugene@titerman.com>
2021-02-11 15:43:06 +0300

    An even shorter version

--- testcafe.js
+++ testcafe.js
@@ -1,12 +1,12 @@
 import 'testcafe';
-import { Selector } from 'testcafe';
 
 fixture `Pizza Palace`
     .page `https://testcafe-demo-page.glitch.me/`;
 
-test('My first test', async t => {
+test('Submit a form', async t => {
     await t
         .setNativeDialogHandler(() => true)
+    
         .drag('.noUi-handle', 100, 0)
         .click('.next-step')
         .click('label[for="pepperoni"]')
@@ -14,15 +14,15 @@ test('My first test', async t => {
         .click('.confirm-address')
         .typeText('#phone-input', '+1-541-754-300')
         .click('#step3 .next-step');
+
     const alertMessage = await t.getNativeDialogHistory();
     await t.expect(alertMessage[0].text).eql('Please enter a valid phone number.')
+    
         .typeText('#phone-input', '1')
-        .click('#step3 .next-step');
-    const restaurantLocation = Selector('.restaurant-location iframe');
-    await t.switchToIframe(restaurantLocation);
-    const zoomInButton = Selector ('#mapDiv').find('button[title="Zoom in"]');
-    await t.click(zoomInButton)
-        .click(zoomInButton)
+        .click('#step3 .next-step')
+
+        .switchToIframe('.restaurant-location iframe')
+        .click('button[title="Zoom in"]')
         .switchToMainWindow()
         .click('.complete-order');
 });
